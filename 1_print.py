88f206ce53b264ed9ec3a492b76d6434876dfaa9
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-01-30 09:54:50 -0500

    :tada: Initial commit

---
+++ 1_print.py
@@ -0,0 +1,17 @@
+print('Hola, esto es Python')
+
+print('Hola soy Francisco y tengo 26 años')
+
+
+# Operaciones matemáticas
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# Comentario en una línea
+
+"""
+Comentario de
+varias lineas
+"""
