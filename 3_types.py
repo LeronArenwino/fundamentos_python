f1837c45c81082a69d9aa93f6d8244f40242e0c7
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-01-31 11:47:18 -0500

    :zap: Data types

---
+++ 3_types.py
@@ -0,0 +1,20 @@
+# String (str)
+my_name = 'Francisco'
+my_name = 'Léron'
+print('My name is ', my_name)
+print(type(my_name))
+
+# Integer (int)
+my_age = 20
+print('My age is ', my_age)
+print(type(my_age))
+
+# Boolean (bool)
+is_single = False
+print(is_single)
+print(type(is_single))
+
+# Input
+my_age = input('How old are you? ')
+print('My age is ', my_age)
+print(type(my_age))
