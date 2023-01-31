40847882fe3520d7b6e8b1d61b4ecf038026900a
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-01-31 09:38:37 -0500

    :zap: Variables class

---
+++ 2_variables.py
@@ -0,0 +1,17 @@
+print('Hola, vars')
+
+# This is a string variable
+my_name = 'Francisco'
+print(my_name)
+
+# This is a numeric variable
+my_age = 20
+print(my_age)
+
+# New value to 'my_name' variable
+my_name = 'Léron'
+print("It's changed",my_name)
+
+# Input function
+my_name = input('What is your name? ')
+print('Using input:', my_name)
